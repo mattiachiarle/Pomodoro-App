33a749acc7ad4133778706e94448f1f0d76b26f0
comacrae <comacrae1995@gmail.com>
2023-10-28 12:29:34 -0500

    Basic layout finished

--- PomodoroApp/App.js
+++ PomodoroApp/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {Text} from 'react-native';
 import {createMaterialBottomTabNavigator} from 'react-native-paper/react-navigation';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -12,41 +12,71 @@ const HomeScreen = () => {
 };
 const SettingsScreen = () => <Text>Settings</Text>;
 const CallsScreen = () => <Text>Calls</Text>;
+const TimerScreen = () => <Text>Timer</Text>;
 const FlashcardsScreen = () => <Text>Flashcards</Text>;
 const ToDoScreen = () => <Text>ToDo</Text>;
 
-function getTabBarIcon({name, focused, color, size}) {
+function getIcon({focused, color, size}, name) {
   let iconName;
-  switch (name) {
-    case 'Home':
-      iconName = 'home';
-      break;
-    case 'ToDo':
-      iconName = 'check';
-      break;
-  }
-  focused ? name : name + '-outline';
-  return <Icon name={iconName} color={color} size={size}></Icon>;
-}
-
-function getScreenOptions({route}) {
-  return {
-    tabBarIcon: getTabBarIcon(route.name, focused, color, size),
-    tabBarActiveTintColor: 'tomato',
-    tabBarInactiveTintColor: 'gray',
-  };
+  iconName = focused ? name : name + '-outline';
+  return (
+    <MaterialCommunityIcons
+      name={iconName}
+      color={color}
+      size={size}></MaterialCommunityIcons>
+  );
 }
 
 function MyTabs() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({route}) => {
-            getScreenOptions(route);
-          }}>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="ToDo" component={ToDoScreen} />
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Timer"
+            component={TimerScreen}
+            options={{
+              tabBarLabel: 'Timer',
+              tabBarIcon: ({focused, color, size}) =>
+                getIcon({focused, color, size}, 'timer'),
+            }}
+          />
+          <Tab.Screen
+            name="Calls"
+            component={CallsScreen}
+            options={{
+              tabBarLabel: 'Calls',
+              tabBarIcon: ({focused, color, size}) =>
+                getIcon({focused, color, size}, 'account-group'),
+            }}
+          />
+          <Tab.Screen
+            name="ToDo"
+            component={ToDoScreen}
+            options={{
+              tabBarLabel: 'To Do',
+              tabBarIcon: ({focused, color, size}) =>
+                getIcon({focused, color, size}, 'check'),
+            }}
+          />
+          <Tab.Screen
+            name="Flashcards"
+            component={FlashcardsScreen}
+            options={{
+              tabBarLabel: 'Flashcards',
+              tabBarIcon: ({focused, color, size}) =>
+                getIcon({focused, color, size}, 'card-text'),
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={SettingsScreen}
+            options={{
+              tabBarLabel: 'Settings',
+              tabBarIcon: ({focused, color, size}) =>
+                getIcon({focused, color, size}, 'cog'),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
