577a7358971f68dd156b215b76366583d89d0d4f
MATTIA CHIARLE s308472 <s308472@studenti.polito.it>
2023-11-07 17:53:43 -0600

    (Draft) FIrst part of call interface

--- PomodoroApp/Calls.js
+++ PomodoroApp/Calls.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text} from 'react-native-paper';
 
 function CallsScreen() {
-  return <Text>Calls</Text>;
+  const [callReady, setCallReady] = useState(false);
+  const [room, setRoom] = useState('');
+
+  const joinCall = () => {
+    setCallReady(true);
+  };
+
+  return (
+    <>
+      {callReady ? (
+        <>
+          <TextInput
+            onChangeText={setRoom}
+            placeholder="Enter room name here"
+            style={{color: 'black', padding: 32}}
+            value={room}
+          />
+          <Button
+            color="blue"
+            disabled={!room}
+            onPress={joinCall}
+            style={{height: 32, width: 32}}
+            title="Join"
+          />
+        </>
+      ) : (
+        <Jitsi />
+      )}
+    </>
+  );
 }
 
 export default CallsScreen;
