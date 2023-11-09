dc1e442b2b12fe0affc3f64225985b235a1be8cd
Colin MacRae <76566137+comacrae@users.noreply.github.com>
2023-11-08 21:25:10 -0600

    implemented buggy timer

--- PomodoroApp/CallsTimerControls.js
+++ PomodoroApp/CallsTimerControls.js
@@ -11,26 +11,89 @@ import {
 } from 'react-native';
 import {useTimer} from 'react-timer-hook';
 
-function TimerEntry() {
-  return (
-    <View style={styles.timerInputWindow}>
-      <View style={styles.timerInputRow}>
-        <Text style={{fontSize: 20, justifyContent: 'center'}}>Name: </Text>
-        <TextInput style={{height: 30, width: 90, borderWidth: 1}}></TextInput>
+function CallsTimerControls({timerState}) {
+  const [showTimerSelection, setShowTimerSelection] = useState(true);
+  const [showTimerInput, setShowTimerInput] = useState(true);
+  const [moduleTime, setModuleTime] = useState(0);
+  const [breakTime, setBreakTime] = useState(0);
+  const [onBreak, setOnBreak] = useState(false);
+  const [moduleName, setModuleName] = useState('');
+
+  function TimerEntry() {
+    return (
+      <View style={styles.timerInputWindow}>
+        <View style={styles.timerInputRow}>
+          <Text style={{fontSize: 20, justifyContent: 'center'}}>Name: </Text>
+          <TextInput
+            style={{height: 40, width: 90, borderWidth: 1}}
+            onChangeText={newText =>
+              setModuleName(parseInt(newText) * 60)
+            }></TextInput>
+        </View>
+        <View style={styles.timerInputRow}>
+          <Text style={styles.timerInputLabel}>Modules:</Text>
+          <TextInput
+            style={styles.timerInputTextbox}
+            onChange={newText => setModuleTime(parseInt(newText) * 60)}
+            keyboardType="numeric"></TextInput>
+          <Text style={styles.timerInputLabel}>mins</Text>
+        </View>
+        <View style={styles.timerInputRow}>
+          <Text style={styles.timerInputLabel}>Breaks: </Text>
+          <TextInput
+            style={styles.timerInputTextbox}
+            keyboardType="numeric"
+            onChangeText={newText =>
+              setBreakTime(parseInt(newText) * 60)
+            }></TextInput>
+          <Text style={styles.timerInputLabel}>mins</Text>
+        </View>
+        <Button title="Start" onPress={() => setShowTimerInput(false)}></Button>
       </View>
-      <View style={styles.timerInputRow}>
-        <Text style={styles.timerInputLabel}>Modules:</Text>
-        <TextInput style={styles.timerInputTextbox}></TextInput>
-        <Text style={styles.timerInputLabel}>mins</Text>
+    );
+  }
+
+  function IconButton({text}) {
+    return (
+      <TouchableOpacity
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-evenly',
+          backgroundColor: 'green',
+          borderWidth: 1,
+          margin: 5,
+        }}
+        onPress={() => {
+          setShowTimerSelection(false);
+        }}>
+        <Image
+          style={{height: 40, width: 40}}
+          source={require('./icons/timer_icon.png')}
+        />
+        <Text style={{fontSize: 20}}>{text}</Text>
+      </TouchableOpacity>
+    );
+  }
+  let output = null;
+  if (showTimerSelection) {
+    output = (
+      <View style={styles.controlsWindow}>
+        <IconButton text={'For you'}></IconButton>
+        <IconButton text={'For everyone'}></IconButton>
       </View>
-      <View style={styles.timerInputRow}>
-        <Text style={styles.timerInputLabel}>Breaks: </Text>
-        <TextInput style={styles.timerInputTextbox}></TextInput>
-        <Text style={styles.timerInputLabel}>mins</Text>
-      </View>
-      <Button title="Start"></Button>
-    </View>
-  );
+    );
+  } else if (!showTimerSelection && showTimerInput) {
+    output = <TimerEntry></TimerEntry>;
+  } else if (!showTimerSelection && !showTimerInput) {
+    const time = new Date();
+    const duration = onBreak ? breakTime : moduleTime;
+    console.log(duration);
+    time.setSeconds(time.getSeconds() + duration);
+    output = <MiniTimer expiryTimestamp={time}></MiniTimer>;
+  }
+  return output;
 }
 
 function MiniTimer({expiryTimestamp}) {
@@ -47,45 +110,22 @@ function MiniTimer({expiryTimestamp}) {
     restart,
   } = useTimer({
     expiryTimestamp,
-    onExpire: () => console.warn('onExpire called'),
+    onExpire: () => {
+      if (!showTimerInput && !onBreak) {
+        setShowTimerInput(true);
+      }
+    },
   });
-}
-
-function CallsTimerControls({timerState}) {
-  const [showTimer, setShowTimer] = useState(true);
-  const [moduleTime, setModuleTime] = useState(0);
-  const [breakTime, setBreakTime] = useState(0);
-
-  function IconButton({text}) {
-    return (
-      <TouchableOpacity
+  return (
+    <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center'}}>
+      <View
         style={{
-          flex: 1,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-evenly',
-          backgroundColor: 'green',
+          width: 100,
+          height: 40,
+          textAlignVertical: 'center',
           borderWidth: 1,
-          margin: 5,
-        }}
-        onPress={() => {
-          setShowTimer(false);
-        }}>
-        <Image
-          style={{height: 40, width: 40}}
-          source={require('./icons/timer_icon.png')}
-        />
-        <Text style={{fontSize: 20}}>{text}</Text>
-      </TouchableOpacity>
-    );
-  }
-  return showTimer ? (
-    <View style={styles.controlsWindow}>
-      <IconButton text={'For you'}></IconButton>
-      <IconButton text={'For everyone'}></IconButton>
+        }}></View>
     </View>
-  ) : (
-    <TimerEntry></TimerEntry>
   );
 }
 
@@ -113,13 +153,13 @@ const styles = StyleSheet.create({
   },
   timerInputLabel: {
     textAlignVertical: 'center',
-    fontSize: 25,
-    width: 110,
+    fontSize: 30,
+    width: 130,
     backgroundColor: 'green',
   },
   timerInputTextbox: {
     width: 30,
-    height: 30,
+    height: 40,
     borderWidth: 1,
   },
 });
